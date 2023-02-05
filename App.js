afa930d988d810c0937704523a54d3a533c2c0ee
Ekta Ahuja <ebhatia680@gmail.com>
2023-02-05 22:27:26 +0530

    submit

--- App.js
+++ App.js
@@ -1,1 +1,21 @@
+const url="https://api.dictionaryapi.dev/api/v2/entries/en/";
+const result= document.getElementById("result");
+const button=document.getElementById("Search-btn");
 
+button.addEventListener("click", () => {
+        let inpword = document.getElementById("inp-word").value;
+        fetch(`${url}${inpword}`)
+        .then((response)=>response.json())
+        .then((data) => {
+            console.log(data)
+            result.innerHTML= `<div class="result" id="result">
+            <div class="Word">
+                <h3>${inpword}</h3>
+            </div>
+            <p class="Word-meaning">${data[0].meanings[0].synonyms}</p>`;
+        })
+    
+        .catch(() => {
+            result.innerHTML=`<h3 class="error">Couldn't find the word</h3>`;
+        });
+    });
